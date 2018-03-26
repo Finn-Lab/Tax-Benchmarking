048312f8fb60cb80df5c4b5e5b9922e40daa24f3
Alexandre Almeida <alexandre.m.s.almeida@gmail.com>
2018-03-26 14:14:22 +0100

    Update and rename comp_usage.R to plot_usage.R

--- R_scripts/plot_usage.R
+++ R_scripts/plot_usage.R
@@ -2,9 +2,7 @@
 library(reshape2)
 library(ggplot2)
 
-Sys.setlocale('LC_ALL','C') # correct warning message
-
-# plot cpu time
+# calculate cpu time
 cpu.mapseq1 = (20048.72/60)/60
 cpu.mapseq2 = (20704.52/60)/60
 cpu.mapseq3 = (21630.30/60)/60
@@ -17,12 +15,9 @@ cpu.mothur3 = ((53.09+15.69+2310.90+599.05+469.67+46.14+0.13+28.14+0.25+56.11+13
 cpu.mothur = mean(cpu.mothur1, cpu.mothur2, cpu.mothur3)
 sd.cpu.mothur = sd(c(cpu.mothur1, cpu.mothur2, cpu.mothur3))
 
-#cpu.qiime1.1 = (160.27/60)/60 # SILVA 97%
-#cpu.qiime1.2 = (87.86/60)/60 # SILVA 97%
-#cpu.qiime1.3 = (93.75/60)/60 # SILVA 97%
-cpu.qiime1.1 = (397.87/60)/60 # SILVA 99%
-cpu.qiime1.2 = (431.05/60)/60 # SILVA 99%
-cpu.qiime1.3 = (493.00/60)/60 # SILVA 99%
+cpu.qiime1.1 = (397.87/60)/60
+cpu.qiime1.2 = (431.05/60)/60
+cpu.qiime1.3 = (493.00/60)/60
 cpu.qiime = mean(cpu.qiime1.1, cpu.qiime1.2, cpu.qiime1.3)
 sd.cpu.qiime = sd(c(cpu.qiime1.1, cpu.qiime1.2, cpu.qiime1.3))
 
@@ -37,7 +32,7 @@ cpu.df = melt(cpu.df)
 cpu.df = cbind(cpu.df, c(sd.cpu.qiime2, sd.cpu.qiime, sd.cpu.mothur, sd.cpu.mapseq))
 colnames(cpu.df) = c("variable", "value", "sd")
 
-# plot bargraph cpu time
+# plot bargraph w/ cpu time
 print(ggplot(cpu.df, aes(x=variable, y=value))
       + geom_bar(stat="identity", fill="darkgray", alpha=0.7, colour="black", size=0.2)
       + geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=0.5, alpha=0.7)
@@ -51,7 +46,7 @@ print(ggplot(cpu.df, aes(x=variable, y=value))
       + theme(axis.title.y = element_blank())
       + theme(axis.title.x = element_text(size=14)))
 
-# plot memory usage
+# calculate memory usage
 mem.mapseq1 = (2743)/1024
 mem.mapseq2 = (2744)/1024
 mem.mapseq3 = (2747)/1024
@@ -64,12 +59,9 @@ mem.mothur3 = (451+630+88172+3447+13316+68+9+494+630+88188+3182)/1024
 mem.mothur = mean(mem.mothur1, mem.mothur2, mem.mothur3)
 sd.mem.mothur = sd(c(mem.mothur1, mem.mothur2, mem.mothur3))
 
-#mem.qiime1.1 = (16462)/1024 # SILVA 97%
-#mem.qiime1.2 = (18761)/1024 # SILVA 97%
-#mem.qiime1.3 = (15171)/1024 # SILVA 97%
-mem.qiime1.1 = (41550)/1024 # SILVA 99%
-mem.qiime1.2 = (41554)/1024 # SILVA 99%
-mem.qiime1.3 = (41565)/1024 # SILVA 99%
+mem.qiime1.1 = (41550)/1024
+mem.qiime1.2 = (41554)/1024
+mem.qiime1.3 = (41565)/1024
 mem.qiime = mean(mem.qiime1.1, mem.qiime1.2, mem.qiime1.3)
 sd.mem.qiime = sd(c(mem.qiime1.1, mem.qiime1.2, mem.qiime1.3))
 
@@ -84,7 +76,7 @@ mem.df = melt(mem.df)
 mem.df = cbind(mem.df, c(sd.mem.qiime2, sd.mem.qiime, sd.mem.mothur, sd.mem.mapseq))
 colnames(mem.df) = c("variable", "value", "sd")
 
-# plot bargraph mem usage
+# plot bargraph w/ mem usage
 print(ggplot(mem.df, aes(x=variable, y=value))
       + geom_bar(stat="identity", fill="darkgray", alpha=0.8, colour="black", size=0.2)
       + geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=0.5, alpha=0.7)
